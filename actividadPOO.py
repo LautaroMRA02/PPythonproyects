76ddfb00a7cc1d7b419d21744b279c857057f7e7
Lautaro Rivadero <79938359+LautaroMRA02@users.noreply.github.com>
2022-06-24 19:56:15 -0300

    me estoy enojando

---
+++ actividadPOO.py
@@ -0,0 +1,182 @@
+## 1
+## 1
+## 1
+from re import I
+
+
+class Persona: 
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_edad(self):
+        return self.edad
+
+    def set_nombre(self,dato):
+         self.nombre = dato
+
+    def set_edad(self,dato):
+         self.edad = dato
+
+    # @staticmethod
+    # def es_mayor_que(person1, person2):
+    #     if person1.edad > person2.edad:
+    #         return f"{person1.nombre} es mayor que {person2.nombre}"
+    #     else:
+    #         return f"{person2.nombre} es mayor que {person1.nombre}"
+    def es_mayor_que(self,otra_persona):
+        if self.edad > otra_persona.edad:
+            return "si es mayor"
+        else:
+            return "No es mayor"
+
+    @staticmethod
+    def get_mayor(persona1,persona2):
+        if persona1.edad > persona2.edad:
+            return f"{persona1.nombre} es mayor que {persona2.nombre}"
+        else:
+            return f"{persona2.nombre} es mayor que {persona1.nombre}"
+
+
+class Alumnos:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+    def __str__(self):
+        return f"nombre: {self.nombre} \nnota: {self.nota}"
+    def aprobo(self):
+        if self.nota > 4:
+            return "Aprobo."
+        else: 
+            return "No aprobo." 
+
+
+class Triangulo:
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def tipo(self):
+        if self.c == self.a == self.b:
+            return "Es Equilatero"
+        elif self.a == self.b or self.a == self.c or self.b== self.c:
+            return "Es Isosceles"
+        else: 
+            return "Es Escaleno" 
+
+
+
+class Calculadora:
+    def __init__(self):
+        self.n1 = int(input()) 
+        self.n2 = int(input())
+        
+    def sumar(self):
+        return self.n1 + self.n1
+
+    def restar(self):
+        return self.n1 - self.n1    
+        
+    def multiplicar(self):
+        return self.n1 * self.n1
+
+    def dividir(self):
+        return self.n1 / self.n1
+
+    def __str__(self):
+        return f"Valores ingresados: {self.n1} {self.n1} \nsuma: {self.sumar()}\nresta: {self.restar()}\nmultipicar: {self.multiplicar()}\ndivicion: {self.dividir()}"
+
+# prueba = Calculadora()
+# print(prueba)
+
+class Agenda:
+    def __init__(self):
+        self.contactos = {}
+        self.id = 0
+
+    def iniciar_app(self):
+        run = True
+        print('Agenda abierta')
+        while run:
+            option = int(input('Opciones:\n1)agregar contacto \n2)eliminar contacto\n3)mostar contactos\n4)editar contacto\n5)cerrar agenda'))
+            if option == 1:
+                self.agregar_contacto()
+            elif option == 2:
+                self.eliminar_contacto()
+            elif option == 3:
+                 self.mostrar_contactos()
+            elif option == 4:
+                self.editar_contacto()
+            elif option == 5:
+                run = False
+                print("app cerrada")
+            else:
+                print("Opcion no valida")
+
+    def agregar_contacto(self):
+        nombre = str(input('ingrese nombre: '))
+        numero = int(input('ingrese numero: '))
+        email = str(input('ingrese email: '))
+        self.id += 1
+        keyid = f'{self.id}id'
+        self.contactos[keyid] = {'nombre': nombre,'numero': numero,'email': email}
+        c=input("presiona Enter para continuar...")
+        print("---------------------------")
+
+
+    def eliminar_contacto(self):
+        if len(self.contactos) <= 0:
+            print("no hay contactos")
+            c=input("presiona Enter para continuar...")
+            print("---------------------------")
+
+        else:
+            eliminar = str(input('eliminar a: '))
+            print()
+            for (key,values) in self.contactos.items():
+
+                if values['nombre'] == eliminar:
+                    del self.contactos[str(key)]
+            #         del self.contactos[key]                    
+            # c=input("presiona Enter para continuar...")
+            # print("---------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+    def editar_contacto(self):
+        editar = str(input('editar a: '))
+        for (key,values) in self.contactos.items():
+            if values['nombre'] == editar:
+                # nombre = str(input('ingrese nombre: '))
+                # numero = int(input('ingrese numero: '))
+                # email = str(input('ingrese email: '))
+                print(values)
+                print(key)
+                del key
+        print("---------------------------")
+    
+
+    def mostrar_contactos(self):
+        print("---------------------------")
+        for (key,values) in self.contactos.items():
+            print("---------------------------")
+            print(f"nombre {values['nombre']}\nnumero: {values['numero']}\nemail:{values['email']}")
+        print("---------------------------")
+        c=input("presiona Enter para continuar...")
+
+
+prueba = Agenda()
+
+prueba.iniciar_app()
